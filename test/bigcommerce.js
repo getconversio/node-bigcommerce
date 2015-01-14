94ea9c6b95ef2f4bec045405a73ea6bad3039723
Rick Craig <rick@spacerevolver.com>
2015-01-14 13:54:59 +0000

    Added tests

---
+++ test/bigcommerce.js
@@ -0,0 +1,13 @@
+var BigCommerce = require('../lib/bigcommerce'),
+  should = require('chai').should(),
+  expect = require('chai').expect();
+
+describe('Constructor: #BigCommerce', function(){
+
+  it('Should return an error if config is missing', function(){
+    should.Throw(function(){
+      new BigCommerce();
+    }, Error);
+  });
+
+})
